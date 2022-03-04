2b20c75056ba21679c731a5609c2281decb983b6
Ryan Shtirmer <rshtirmer@gmail.com>
2022-03-04 16:04:16 -0500

    formatting

--- src/modules/ethereum/ethereum.service.ts
+++ src/modules/ethereum/ethereum.service.ts
@@ -34,7 +34,7 @@ export class EthereumService {
     this.ether = ethersProvider;
   }
 
-  public async getBlockNum(){
+  public async getBlockNum() {
     return this.ether.getBlockNumber();
   }
 }
