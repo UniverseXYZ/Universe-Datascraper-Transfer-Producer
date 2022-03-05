bab73043780808878d6e4ef62ee6f274adcdf303
Ryan Shtirmer <rshtirmer@gmail.com>
2022-03-05 15:02:42 -0500

    Update ethereum.service.ts

--- src/modules/ethereum/ethereum.service.ts
+++ src/modules/ethereum/ethereum.service.ts
@@ -8,37 +8,41 @@ export class EthereumService {
   public ether: ethers.providers.BaseProvider;
 
   constructor(private configService: ConfigService) {
-    const network: ethers.providers.Networkish =
-      this.configService.get('ethereum_network');
-    const quorum = Number(this.configService.get('ethereum_quorum'));
+    const network: ethers.providers.Networkish = this.configService.get('ethereum_network');
+    const quorum: number = Number(this.configService.get('ethereum_quorum'));
 
-    const projectSecret: string = this.configService.get(
-      'infura.project_secret',
-    );
+    const projectSecret: string = this.configService.get('infura.project_secret');
     const projectId: string = this.configService.get('infura.project_id');
-    const infura: InfuraProject =
-      projectId && projectSecret ? { projectId, projectSecret } : undefined;
+    const infuraProvider: ethers.providers.InfuraProvider = projectId && projectSecret
+      ? new ethers.providers.InfuraProvider(network, {
+          projectId: projectId,
+          projectSecret: projectSecret
+        })
+      : undefined;
+        
+    const alchemyToken: string = this.configService.get('alchemy_token')
+    const alchemyProvider: ethers.providers.AlchemyProvider = alchemyToken
+      ? new ethers.providers.AlchemyProvider(network, {
+          apikey: alchemyToken,
+        })
+      : undefined;
 
-    const alchemyToken: string = this.configService.get('alchemy_token');
-    const alchemy: string = alchemyToken ? alchemyToken : undefined;
+    const chainstackUrl: string = this.configService.get('chainstack_url')
+    const chainStackProvider: ethers.providers.JsonRpcProvider = chainstackUrl
+      ? new ethers.providers.JsonRpcProvider(chainstackUrl, network)
+      : undefined;
 
-    if (!infura && !alchemy) {
-      throw new Error(
-        'Infura project id and secret or alchemy token is not defined',
-      );
+    if (!infuraProvider && !alchemyProvider && !chainStackProvider) {
+      throw new Error('Infura project id and secret or alchemy token or chainstack url is not defined');
     }
+    
+    const allProviders: ethers.providers.BaseProvider[] = [infuraProvider, alchemyProvider, chainStackProvider]
+    const definedProviders: ethers.providers.BaseProvider[] = allProviders.filter(x => x !== undefined);
 
-    const opts: ProviderOptions = {
-      quorum: quorum,
-      alchemy: alchemy,
-      infura: infura,
-    };
-
-    const ethersProvider: ethers.providers.BaseProvider =
-      ethers.getDefaultProvider(network, opts);
+    const ethersProvider: ethers.providers.FallbackProvider = new ethers.providers.FallbackProvider(definedProviders, quorum);
     this.ether = ethersProvider;
   }
-
+  
   public async getBlockNum() {
     return this.ether.getBlockNumber();
   }
